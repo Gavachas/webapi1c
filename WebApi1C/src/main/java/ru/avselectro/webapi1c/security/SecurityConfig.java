85c5cc8a3a2ab4678c53b4fea5fe567aca125c2b
EvgeniiKochetov <KochetovEvgeny@gmail.com>
2021-11-09 23:34:56 +0300

    Step 2 customauthentication

--- WebApi1C/src/main/java/ru/avselectro/webapi1c/security/SecurityConfig.java
+++ WebApi1C/src/main/java/ru/avselectro/webapi1c/security/SecurityConfig.java
@@ -34,18 +34,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-		//customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+		customAuthenticationFilter.setFilterProcessesUrl("/api/login");
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
-		http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
-		/*http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
+		http.authorizeRequests().antMatchers("/api/login/**").permitAll();
 		http.authorizeRequests().antMatchers("GET", "/api/users/**").hasAnyAuthority("ROLE_USER");
 		http.authorizeRequests().antMatchers("POST", "/api/users/save/**").hasAnyAuthority("ROLE_ADMIN");
-		http.authorizeRequests().anyRequest().authenticated();*/
-		//http.addFilter(customAuthenticationFilter);
-		//http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.authorizeRequests().anyRequest().authenticated();
+		http.addFilter(customAuthenticationFilter);
+		
 	}
 	@Bean
 	@Override
